601312d6aa07c57e9575676189d7fc2f751fe8b1
SantMu123 <smunozcastaneda14@gmail.com>
2024-09-20 16:51:56 -0500

    feat: :wrench: to animate image config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fade: {
+          '0%, 100%': { opacity: 0 },
+          '50%': { opacity: 1 },     
+        },
+      },
+      animation: {
+        'fade-in-out': 'fade 4s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
