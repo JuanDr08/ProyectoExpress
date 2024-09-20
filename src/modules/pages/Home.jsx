5395fe4efdb9c9ba394dbf07121448acb0008cec
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-09-20 17:34:32 -0500

    feat: :sparkles: Header component

---
+++ src/modules/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Muesca } from "../components/Muesca"
+import { CategoryHeaders } from "../components/CategoryHeaders"
+
+export const Home = () => {
+
+    return (
+        <>
+            <Muesca />
+            <CategoryHeaders title="Categorias" />
+        </>
+    )
+
+}
