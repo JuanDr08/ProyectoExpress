a92a969f26c31e2ef7acdb8e2ca42513d8ccca97
SantMu123 <smunozcastaneda14@gmail.com>
2024-09-23 17:40:18 -0500

    feat: :sparkles: information craft view done

--- src/modules/pages/InfoCraft.jsx
+++ src/modules/pages/InfoCraft.jsx
@@ -3,31 +3,30 @@ import { Muesca } from '../components/Muesca';
 
 function InfoCraft() {
   return (
-    <div className="relative flex flex-col items-center w-full h-full space-y-8 p-8">
+    <div className="relative flex flex-col justify-center items-center w-full h-full ">
       {/* Muesca en la esquina superior izquierda */}
       <div className="absolute top-0 left-0">
         <Muesca />
       </div>
 
-      {/* Sección superior con rhombuses y texto */}
-      <div className="flex flex-col items-center w-full mb-20 space-y-4">
-        <img src="/img/rhombuses.png" className="w-60 h-auto" alt="Rhombuses" />
-        <img src="/img/TextInfoCraft.png" className="w-80 h-auto" alt="Text Info" />
-        <img src="/img/rhombuses.png" className="w-60 h-auto" alt="Rhombuses" />
-    </div>
+      <div className="relative flex flex-col items-center justify-center w-full mb-20 mt-[5em] h-[5em]">
+        <img src="/img/rhombusesTitle.png" className="w-[20em] h-auto" alt="Rhombuses" />
+      </div>
 
+      <div className='text-left mb-2'>
+        <h3 style={{ color: '#9D1A1A' }} className='text-[1em]'>Taller de arte Awaq Ayllus - Documental</h3>
+      </div>
 
-      {/* Sección de video (centrada con flex) */}
-      <div className="flex items-center justify-center bg-[#703A31] p-4 rounded-lg relative mt-10 w-full">
-        <img src="img/VideoPhoto.png" className="w-60 h-auto" alt="Video Preview" />
-        <img src="img/ButtonPlay.png" className="absolute w-10 h-auto" alt="Play Button" />
+      <div className="flex items-center justify-center bg-[#703A31] px-4 relative  w-full h-[16em]">
+        <img src="img/VideoPhoto.png" className="w-full h-auto" alt="Video Preview" />
+        <img src="img/ButtonPlay.png" className="absolute w-[5em] h-auto" alt="Play Button" />
       </div>
 
       {/* Información interactiva */}
-      <div className="flex flex-col items-center text-center space-y-4">
-        <p className="text-lg font-semibold">Conoce más del taller de forma interactiva</p>
+      <div className="flex flex-col items-center text-left space-y-4 mx-6 mt-[2em]">
+        <p style={{ color: '#9D1A1A' }} className="text-lg font-semibold text-left">Conoce más del taller de forma interactiva</p>
         <p className="text-base">Escanea el código QR con tu celular y disfruta de la experiencia</p>
-        <img src="/img/QRPhoto.png" className="w-40 h-auto mt-10" alt="QR Code" />
+        <img src="/img/QRPhoto.png" className="w-[12em] h-auto mt-10" alt="QR Code" />
       </div>
     </div>
   );
