5116f0164bb369e784d6ac117cb35f44665f4263
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-09-22 20:04:40 -0500

    feat: :sparkles: Page #10 finished, exposed to changes

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -4,11 +4,12 @@ import {
 } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { CraftWorkshops } from "./pages/Craft_Workshops";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home/>,
+        element: <CraftWorkshops/>,
     }
 ]);
 
