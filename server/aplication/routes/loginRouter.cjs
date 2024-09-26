8e91cc81f4909e71f5c78ff9a770456837246d8f
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-09-26 17:47:20 -0500

    feat: :alembic: Testing passport

--- server/aplication/routes/loginRouter.cjs
+++ server/aplication/routes/loginRouter.cjs
@@ -1,21 +1,35 @@
 const passport = require('passport');
-// const configPassportGoogleOAuth = require('../middlewares/googleOAuth');
-const authController = require('../controllers/loginController.cjs');
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-const UserController = require('../controllers/userController.cjs');
-const userController = new UserController();
 
-// configPassportGoogleOAuth(passport)
+// Manejadores de la autenticacion con google
+const configPassportGoogleOAuth = require('../middlewares/googleOAuth.cjs');
+const { googleAuthCallback } = require('../controllers/loginController.cjs')
 
 
+configPassportGoogleOAuth(passport) // Configuramos la estrategia de autenticacion de google
 
+
+router.get('/', (req, res, next) => {
+    console.log(req.session)
+    res.send('HOLA')
+})
 
 // router.post('/auth/user', express.urlencoded({ extended: true }), (req, res) => userController.verifyUser(req, res))
 
-// router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
-// router.get('/auth/google/callback',  authController.googleAuthCallback)
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+router.get('/auth/google/callback', googleAuthCallback )
+router.get('/si', (req,res) => {
+    console.log(req.user)
+})
 
+router.get('/logout', (req, res)=>{
+    req.logOut((err) => {
+        if (err) {
+            console.error('error', err)
+        }
+        console.log(req.session)
+    })
+})
 
 module.exports = router;
--- server/infrastructure/server/server.cjs
+++ server/infrastructure/server/server.cjs
@@ -1,30 +1,27 @@
-// Configuración y puesta en marcha del servidor Express.
 const express = require('express');
 const passport = require('passport');
 const sessionGoogleOAuth = require('../middlewares/sessionOAuth.cjs');
-const path = require('path');
 const indexRouter = require('../../aplication/routes/indexRouter.cjs');
-// const loginRouter = require('../../application/routes/loginRouter');
-// const createAccountRouter = require('../../application/routes/createAccountRouter');
-// const userRoutes = require('../../application/routes/userRoutes');
-// const productRoutes = require('../../application/routes/productRoutes');
+const loginRouter = require('../../aplication/routes/loginRouter.cjs');
 const { jsonParseErrorHandler } = require('../middlewares/errorHandling.cjs');
 const { limiTotal } = require('../middlewares/rateLimit.cjs');
 
-const currentDirectory = process.cwd();
-let EXPRESS_STATIC = currentDirectory + '/src'
 
 const createServer = () => {
     const app = express();
     
-    app.use(jsonParseErrorHandler);    
+    app.use(jsonParseErrorHandler);
     app.use(limiTotal);
   
-    app.use('/', indexRouter);
-    // app.use('/login', sessionGoogleOAuth, passport.initialize(), passport.session(), loginRouter);
-    // app.use('/createAccount', createAccountRouter);
-    // app.use('/users', userRoutes);
-    // app.use('/home', productRoutes);
+    app.use('/',  sessionGoogleOAuth, passport.initialize(), passport.session(),(req, res, next) => {
+        console.log(req.session, 'login') 
+        next()
+    } , indexRouter);
+    app.use('/login', sessionGoogleOAuth, passport.initialize(), passport.session(), loginRouter);
+    app.use('/si', (req, res, next) => {
+        console.log(req, req.session)
+        next()
+    })
     return app;
 };
 
